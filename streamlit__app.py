d39eaf2bf0ebe4de5580761a69871d96e7efcb7a
Chenchugari Rahul <85639865+rahulchenchugari@users.noreply.github.com>
2025-01-29 15:30:22 +0530

    Update streamlit__app.py

--- streamlit__app.py
+++ streamlit__app.py
@@ -31,7 +31,9 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        st.write(f"Fetching from: https://fruityvice.com/api/fruit/{search_on}")  # Debugging
+        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{search_on}")
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
         # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         # st_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width=True)
